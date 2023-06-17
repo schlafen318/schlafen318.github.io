5044a095cff7800d71609674c72a9a6549973f9a
schlafen318 <38609606+schlafen318@users.noreply.github.com>
2023-06-17 08:10:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,53 +1,22 @@
-let timer = {
-    time: 25 * 60, // initial time in seconds
-    running: false, // flag indicating if the timer is running
+// Create a new worker
+let worker = new Worker('worker.js');
 
-    start: function() {
-        if (!this.running) {
-            this.running = true;
-            this.tick();
-        }
-    },
-
-    pause: function() {
-        this.running = false;
-    },
-
-    reset: function() {
-        this.time = 25 * 60; // reset time to initial value
-        this.running = false;
-        this.updateDisplay();
-    },
-
-    tick: function() {
-        if (this.running) {
-            this.time--;
-            if (this.time >= 0) {
-                this.updateDisplay();
-                setTimeout(this.tick.bind(this), 1000); // schedule the next tick
-            } else {
-                this.running = false; // stop the timer when it reaches 0
-            }
-        }
-    },
-
-    updateDisplay: function() {
-        let minutes = Math.floor(this.time / 60);
-        let seconds = this.time % 60;
-        // assume that there is an HTML element with id "timer"
-        document.getElementById('timer').innerText = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-    }
+// Handle messages from the worker
+worker.onmessage = function(event) {
+    // Update the timer display with the data from the worker
+    document.getElementById('timer').innerText = event.data;
 };
 
-// assume that there are HTML elements with ids "start", "pause", and "reset"
+// Add event listeners for the buttons
 document.getElementById('start').addEventListener('click', function() {
-    timer.start();
+    worker.postMessage('start');
 });
 
 document.getElementById('pause').addEventListener('click', function() {
-    timer.pause();
+    worker.postMessage('pause');
 });
 
 document.getElementById('reset').addEventListener('click', function() {
-    timer.reset();
+    worker.postMessage('reset');
 });
+
